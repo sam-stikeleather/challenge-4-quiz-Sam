6148ee1de0e1979e59fd5637171f3ccbcfe77a5c
Sam <sam.stikeleather@gmail.com>
2023-06-22 17:46:05 -0400

    added quiz questions to JS

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+
+//Quiz Questions
+const quizQuestions = [
+    {
+        question: "What does CSS stand for?",
+        options: ["Cascading Style Sheets", "Computer Style Stuff", "Calculating Skill Show", "Cats Shouldn't Sing"],
+        answer: 0
+    },
+    {
+        question: "An 'if then' statement could be simplified using which two symbols?",
+        options: ["? :", "! @", "# $", "& *"],
+        answer: 0
+    },
+    {
+        question: "The HT in HTML stands for what?",
+        options: ["HyperText", "HighTest", "HungaryTurkey", "HypotheticalTerms"],
+        answer: 0
+    },
+    {
+        question: "Who invented the internet?",
+        options: ["Al Gore", "Bill Gates", "Steve Jobs", "Tim Berners-Lee"],
+        answer: 3
+    },
+    {
+        question: "The .md in README.md is short for what?",
+        options: ["ManDark", "MoreData", "MarkDown", "MetaData"],
+        answer: 2
+    }
+];
+
+
